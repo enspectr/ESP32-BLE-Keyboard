42b7d35eab0745cf9af2d4c83b4979b521c1deba
Oleg Volkov <olegv142@gmail.com>
2025-12-23 19:13:04 +0300

    Use plain delay instead of busy loop

--- BleKeyboard.cpp
+++ BleKeyboard.cpp
@@ -495,12 +495,5 @@ void BleKeyboard::onWrite(BLECharacteristic* me, NimBLEConnInfo & connInfo) {
 }
 
 void BleKeyboard::delay_ms(uint64_t ms) {
-  uint64_t m = esp_timer_get_time();
-  if(ms){
-    uint64_t e = (m + (ms * 1000));
-    if(m > e){ //overflow
-        while(esp_timer_get_time() > e) { }
-    }
-    while(esp_timer_get_time() < e) {}
-  }
+	delay(ms);
 }
